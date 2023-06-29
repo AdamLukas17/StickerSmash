8f00882f3021fb6934a0250a8971d653d4d94a3d
Adam Lukas <adammolina17@gmail.com>
2023-06-28 21:38:19 -0400

    added buttons and fixed app.js issue

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -26,6 +28,18 @@ export default function App() {
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
   return (
     <View style={styles.container}>
       {/* MAIN IMAGE */}
@@ -34,15 +48,21 @@ export default function App() {
         placeholderImageSource={PlaceholderImage}
         selectedImage={selectedImage} />
       </View>
+
       {showAppOptions ? (
-        <View />
+       <View style={styles.optionsContainer}>
+       <View style={styles.optionsRow}>
+         <IconButton icon="refresh" label="Reset" onPress={onReset} />
+         <CircleButton onPress={onAddSticker} />
+         <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+       </View>
+     </View>
       ) : (
-        {/* BUTTONS */}
         <View style={styles.footerContainer}>
-          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-        </View>
-    )}
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+      </View>
+      )}
       {/* Handles the statusBar on device */}
       <StatusBar style="auto" />
     </View>
@@ -62,5 +82,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
